0a67b3736afb13b0f1e648d6a5bf6b0bda5a328c
EvelinBermeo <evelinbermeo05@gmail.com>
2022-03-17 01:03:01 -0500

    nuevo

--- Ejercicio_MercadoLibre/js/MercadoLibre.js
+++ Ejercicio_MercadoLibre/js/MercadoLibre.js
@@ -12,10 +12,11 @@ class Automovil {
 
 let auto1 = new Automovil("Toyota","4Runner",2018,"img/Toyota4runner.jpg", 245000000, 81700, "Medellín");
 let auto2 = new Automovil("Toyota","Prado 3.0 Tx",2011, "img/ToyotaPrado.jpg",133000000 ,176000, "Bogotá");
+let auto3 = new Automovil("Toyota","Prado 3.0 Tx",2011, "img/ToyotaPrado.jpg",133000000 ,176000, "Bogotá");
 
-/* mostrarInformacion(auto1);*/
 
-function mostrarVehiculo(auto) {
+
+function mostrarAuto(auto) {
     let contenedor = document.getElementById("main-content")
     let contenedorImg = document.createElement("div");
     contenedor.appendChild(contenedorImg);
@@ -27,7 +28,7 @@ function mostrarVehiculo(auto) {
     contenedorImg.appendChild(imgAuto);
 
     let modeloAuto = document.createElement("label");
-    let textomodeloAuto = document.createTextNode(auto.marca + " " + auto.modelo);
+    let textomodeloAuto = document.createTextNode(auto.marca + " " + auto.modelo + " - " + auto.ciudad );
     modeloAuto.appendChild(textomodeloAuto);
     modeloAuto.setAttribute("class", "style-modelo");
     contenedorImg.appendChild(modeloAuto);
@@ -44,7 +45,7 @@ function mostrarVehiculo(auto) {
     anyoAuto.appendChild(textoanyoAuto);
 
     let kilometrajeAuto = document.createElement("label");
-    let textokilometrajeAuto = document.createTextNode(auto.anyo + " " + auto.ciudad + " - " + auto.kilometraje + "km");
+    let textokilometrajeAuto = document.createTextNode(auto.anyo + " - " + auto.kilometraje + "km");
     kilometrajeAuto.appendChild(textokilometrajeAuto);
     kilometrajeAuto.setAttribute("class", "style-kilometraje");
     contenedorImg.appendChild(kilometrajeAuto);
@@ -54,27 +55,25 @@ function mostrarVehiculo(auto) {
 
 }
 
-// //Esto es temporal
-// function mostrarInfo() {
-//     mostrarInformacion(auto1);
-// }
+
 window.addEventListener("keydown", function(event) {
-    let busqueda = document.getElementById("busqueda").value;
+    let busqueda = document.getElementById("buscando").value;
     if(event.key == "Enter") {
-        limpiarVentana();
+        limpiar();
         if (busqueda == "Toyota") {
-            mostrarVehiculo(auto1);
-            mostrarVehiculo(auto2);
+            mostrarAuto(auto1);
+            mostrarAuto(auto2);
+            mostrarAuto(auto3);
         }
         else if (busqueda == "Toyota Corolla") {
-            mostrarVehiculo(auto1);
+            mostrarAuto(auto2);
         }
         else {
         this.alert("No se han encontrado coincidencias.");
         }
     }
 });
-function limpiarVentana() {
+function limpiar() {
     document.getElementById("main-content").innerHTML = "";
 }
 
